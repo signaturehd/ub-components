4c502a5bc7d153972763f23a803c089ab5c7286c
Mark Anthony Sevilla Parcia <parciamarkanthony@gmail.com>
2018-04-17 21:13:27 +0800

    appbar

--- AppBar/AppBar.js
+++ AppBar/AppBar.js
@@ -3,24 +3,59 @@ import PropTypes from 'prop-types'
 
 import './styles/appbar.css'
 
-
-class SideBar extends Component {
-  constructor(props) {
+class AppBar extends Component {
+  constructor (props) {
     super(props)
   }
 
-  render() {
+  render () {
 
+    const { onClick, title, description, image, author, rating } = this.props
+
+    const style = {
+      cardStyle : {
+        background : `url(${image}) rgba(0,0,0,0.4)`,
+        backgroundSize : "contain",
+        height: "20px"
+      }
+    }
     return (
-      <header>
-        <img
-        src={ require('../../images/logo.png') }
-        className= {'_img-ub-logo'}
-        />
-        <div className = {'_appbar-contents'}>
+      <div className={'header'}>
+        <div className = {'icon-header'}>
+          <img
+            src={ require('../../../../images/logo.png') }
+            className= {'_img-ub-logo'}/>
         </div>
-      </header>
+        {
+          this.state.mobileView &&
+        <a className= {'_round-button'}
+          onClick={this.toggleSideBar}>
+          <img
+            src={ require('../../../../images/profile-picture.png')}
+            className = {'_img-ub-logo'} >
+          </img>
+        </a>
+      }
+      </div>
     )
   }
 }
-export default SideBar
+
+AppBar.propTypes = {
+  onClick : PropTypes.func,
+  title : PropTypes.string,
+  description : PropTypes.string,
+  image : PropTypes.string,
+  author : PropTypes.string,
+  rating : PropTypes.number,
+}
+
+AppBar.defaultProps = {
+  title : 'title',
+  description : 'description',
+  author : "author",
+  image : "image",
+  rating : 1,
+}
+
+export {AppBar}
