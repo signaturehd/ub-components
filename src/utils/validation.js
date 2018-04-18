75ed4f0bbee49aa8a97313e2c924f5e4908f6cf2
Noel <noel.simborios@gmail.com>
2018-04-18 13:46:24 +0800

    Merge branch 'masterubcomponents' of C:\Users\noels\Desktop\hr-benefits-web-v2 with conflicts.

---
+++ src/utils/validation.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default function emailValidation(email){
+
+    const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+    return regex.test(String(email).toLowerCase())
+}
+
+export default function accntnumberValidation(accntnumber){
+    const regex=  /^(?=[\d-]{5,24}$)\d{1,6}-\d*[-]?\d+$ /
+
+}
+
