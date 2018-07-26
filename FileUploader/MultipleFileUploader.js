100a7e29870887d93719149aff536f221f5bf495
Mark Anthony Sevilla Parcia <parciamarkanthony@gmail.com>
2018-07-26 23:19:12 +0800

    Updated multiple file uploader for ub components

--- FileUploader/MultipleFileUploader.js
+++ FileUploader/MultipleFileUploader.js
@@ -5,10 +5,12 @@ import PropTypes from 'prop-types'
 class MultipleFileUploader extends Component {
   constructor (props) {
     super(props)
+      this.state = {
+        attachmentArray : []
+    }
   }
 
   fileChecker (file) {
-
   }
 
   isValid (attachment) {
@@ -36,23 +38,78 @@ class MultipleFileUploader extends Component {
       disabled
     } = this.props
 
+    const {
+      attachmentArray
+    } = this.props
+
     return (
       <div>
-        <h4>{ placeholder }</h4>
+        <center>
+          <h4 className = { 'font-weight-bolder' }>{ placeholder }</h4>
+        </center>
         {
           fileArray.length !== 0 &&
           fileArray.map((attachment, key) => (
             <div key = {key}>
-              <br/>
+              <h2 className = { 'font-size-14px' }>{ attachment.name }</h2>
               <GenericFileInput
-                text = { attachment.name }
-                onChange = { (e) => setFile(e.target.files[0], key)}
+                onChange = { (e) => {
+                  const updatedAttachment = [...fileArray]
+                  const reader = new FileReader()
+                  const file = e.target.files[0]
+                  reader.onloadend=() => {
+                    updatedAttachment[key].base64 = reader.result
+                    updatedAttachment[key].file = file
+                    this.setState({ fileArray : updatedAttachment })
+                  }
+                  reader.readAsDataURL(file)
+                  }
+                }
                 disabled = { disabled }
                 errorMessage = {
                   attachment &&
                   attachment.file &&
                   this.isValid(attachment.file.type) ? 'Invalid File' : '' }
               />
+                <div>
+                  <div className={ 'multiple-file-attachment-form' }>
+                    <img
+                      src={ require('./images/x-circle.png') }
+                      className={ 'close-button' }
+                      onClick={
+                        () => {
+                          const updatedAttachment = [...fileArray]
+                          updatedAttachment[key].base64 = ''
+                          updatedAttachment[key].file = ''
+                          this.setState({ fileArray : updatedAttachment })
+                        }
+                      }
+                    />
+                  {
+                    attachment.base64 ?
+                    <div
+                      style={ {
+                        backgroundImage: `url('${attachment && attachment.base64}')`,
+                        width: 'auto',
+                        height: '60px',
+                        backgroundSize: 'contain',
+                        backgroundRepeat: 'no-repeat',
+                      } }
+                    >
+                      <h6
+                        className={ 'mpl-file-name' }>
+                        {
+                          attachment &&
+                          attachment.file &&
+                          this.isValid(attachment.file.type) ?
+                              attachment.name : '' }
+                      </h6>
+                    </div>
+                    :
+                    <div></div>
+                  }
+                </div>
+              </div>
             </div>
           ))
         }
