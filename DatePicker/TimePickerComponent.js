c988e7120fc857c5bafdc274f253fb6783ea208a
Mark Anthony Sevilla Parcia <parciamarkanthony@gmail.com>
2018-09-03 15:55:44 +0800

    Updated time picker for timepicker

--- DatePicker/TimePickerComponent.js
+++ DatePicker/TimePickerComponent.js
@@ -7,58 +7,58 @@ import './css/datepicker.css'
 import 'react-times/css/material/default.css'
 class TimePickerComponent extends Component {
 
-  render() {
-    const {
-      time,
-      text,
-      errorMessage,
-      disabled,
-      readOnly,
-      timeMode,
-      showTimezone,
-      timeFormat,
-      onTimeChange,
-      meridiem
-    } = this.props
+ render() {
+   const {
+     time,
+     text,
+     errorMessage,
+     disabled,
+     readOnly,
+     timeMode,
+     showTimezone,
+     timeFormat,
+     onTimeChange,
+     meridiem
+   } = this.props
 
-    return (
-      <div className='datepicker-container'>
-        <span className='datepicker-text'>{ text }</span>
-        <TimePicker
-          text = { text }
-          format = { timeFormat }
-          timeMode = { timeMode }
-          meridiem = { meridiem }
-          time = { time }
-          onTimeChange = { onTimeChange }
-          disabled = { disabled }
-        />
-        <div className = { 'date-grid-wrapper' }>
-          <div></div>
-          {
-            errorMessage ?
-            <span className = { 'error-message' }>
-              { errorMessage }
-            </span>
-            :
-            <span className = { 'error-message-null' }></span>
-          }
-        </div>
-      </div>
-    )
-  }
+   return (
+     <div className='datepicker-container'>
+       <span className='datepicker-text'>{ text }</span>
+       <TimePicker
+         text = { text }
+         format = { timeFormat }
+         timeMode = { timeMode }
+         meridiem = { meridiem }
+         time = { time }
+         onTimeChange = { onTimeChange }
+         disabled = { disabled }
+       />
+       <div className = { 'date-grid-wrapper' }>
+         <div></div>
+         {
+           errorMessage ?
+           <span className = { 'error-message' }>
+             { errorMessage }
+           </span>
+           :
+           <span className = { 'error-message-null' }></span>
+         }
+       </div>
+     </div>
+   )
+ }
 }
 
 TimePickerComponent.propTypes = {
-  selected: PropTypes.object,
-  onTimeChange: PropTypes.func,
-  text: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.object,
-  ]),
-  errorMessage: PropTypes.string,
-  timeMode: PropTypes.string,
-  disabled : PropTypes.bool,
+ selected: PropTypes.object,
+ onTimeChange: PropTypes.func,
+ text: PropTypes.oneOfType([
+   PropTypes.string,
+   PropTypes.object,
+ ]),
+ errorMessage: PropTypes.string,
+ timeMode: PropTypes.string,
+ disabled : PropTypes.bool,
 }
 
 export default TimePickerComponent
