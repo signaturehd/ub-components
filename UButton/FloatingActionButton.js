87f1aa037da738d853255dc6c4b58b7f444acd45
Karen Joy D. Del Rosario <kjddelrosario@core.unionbank.com>
2018-10-25 14:15:22 +0800

    added image in floatbutton

--- UButton/FloatingActionButton.js
+++ UButton/FloatingActionButton.js
@@ -1,17 +1,19 @@
 import React , { Component } from 'react'
 
 import './styles/button.css'
+import Image from '../../images/drawer/mytravel.png'
 
 class FloatingActionButton extends Component {
   render () {
-    const { text, className, onClick } = this.props
+    const { text, className, image, onClick } = this.props
 
     return (
       <button
         className = { `_btn-fab ${className}` }
         onClick = {onClick}
       >
-      {text}
+      {text && text}
+      {image && <img src={Image} width = { '60%' } height = { '60%' }/>}
       </button>
 
     )
